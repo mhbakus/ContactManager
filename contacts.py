ed041b7304ed98d51e9c59c2f86a3f6126e756a3
Bakayoko Mohamed <mowamedbakus@gmail.com>
2017-09-18 09:36:05 +0000

    creating a class called contact

---
+++ contacts.py
@@ -0,0 +1,8 @@
+class Contacts():
+    def __init__(self, name, phone, email, website, birthday, linkedin):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.website = website
+        self.birthday = birthday
+        self.linkedin = linkedin
