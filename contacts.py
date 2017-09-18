56ff789cf89a96922fbfae413b76f603897e9348
Bakayoko Mohamed <mowamedbakus@gmail.com>
2017-09-18 09:52:22 +0000

    fuxing bug on method get_contact

--- contacts.py
+++ contacts.py
@@ -7,17 +7,18 @@ class Contacts():
         self.birthday = birthday
         self.linkedin = linkedin
 
-    def get_contact():
-        print("Name : {}\n
+    def get_contact(self):
+        print('''
+               Name : {}\n
                Phone : {}\n
                Email : {}\n
                Website : {}\n
                Birthday : {}\n
-               Linkedin{}".format(self.name,
+               Linkedin{}'''.format(self.name,
                self.phone,
                self.email,
                self.website,
                self.birthday,
                self.linkedin ))
 jean = Contacts('jean', '+22545785896', 'jean@mail.fr', 'www.jean.com', '15/08/1990', 'linkedin.com/in/jean')
-jean.get_contacts()
+jean.get_contact()
